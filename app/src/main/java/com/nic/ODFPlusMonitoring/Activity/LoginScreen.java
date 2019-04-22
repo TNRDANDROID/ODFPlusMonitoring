30673c043aec246a72ea63456b7d43ceed523775
AchanthiSundar <m.s.achanthisundar@gamil.com>
2019-04-22 18:06:10 +0530

    Initial commit

---
+++ app/src/main/java/com/nic/ODFPlusMonitoring/Activity/LoginScreen.java
@@ -0,0 +1,348 @@
+package com.nic.ODFPlusMonitoring.Activity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+
+import com.nic.ODFPlusMonitoring.Api.Api;
+import com.nic.ODFPlusMonitoring.Api.ApiService;
+import com.nic.ODFPlusMonitoring.Api.ServerResponse;
+import com.nic.ODFPlusMonitoring.Constant.AppConstant;
+import com.nic.ODFPlusMonitoring.R;
+import com.nic.ODFPlusMonitoring.Session.PrefManager;
+import com.nic.ODFPlusMonitoring.Support.MyEditTextView;
+import com.nic.ODFPlusMonitoring.Utils.FontCache;
+import com.nic.ODFPlusMonitoring.Utils.UrlGenerator;
+import com.nic.ODFPlusMonitoring.Utils.Utils;
+import com.scottyab.showhidepasswordedittext.ShowHidePasswordEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import DataBase.DBHelper;
+
+/**
+ * Created by AchanthiSundar on 28-12-2018.
+ */
+
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener, Api.ServerResponseListener {
+
+    private Button login_btn,signUp_btn,register_btn;
+    private String name, pass, randString;
+
+    EditText userName, password, reg_username, reg_password,
+            reg_firstName, reg_lastName, reg_email, reg_confirmemail;
+
+    TextInputLayout txtInLayoutUsername, txtInLayoutPassword, txtInLayoutRegPassword;
+    CheckBox rememberMe;
+    private ShowHidePasswordEditText passwordEditText;
+    public static DBHelper dbHelper;
+    public static SQLiteDatabase db;
+    JSONObject jsonObject;
+
+    String sb;
+    private PrefManager prefManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        setContentView(R.layout.activity_main);
+        try {
+            dbHelper = new DBHelper(this);
+            db = dbHelper.getWritableDatabase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        intializeUI();
+    }
+
+    public void intializeUI() {
+        prefManager = new PrefManager(this);
+        login_btn = (Button) findViewById(R.id.login);
+        signUp_btn = (Button) findViewById(R.id.signUp);
+        userName = (EditText) findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        txtInLayoutUsername = findViewById(R.id.txtInLayoutUsername);
+        txtInLayoutPassword = findViewById(R.id.txtInLayoutPassword);
+        rememberMe = findViewById(R.id.rememberMe);
+
+//        passwordEditText = (ShowHidePasswordEditText) findViewById(R.id.password);
+
+
+        login_btn.setOnClickListener(this);
+        signUp_btn.setOnClickListener(this);
+
+//        passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+//        inputLayoutEmail.setTypeface(FontCache.getInstance(this).getFont(FontCache.Font.REGULAR));
+//        inputLayoutPassword.setTypeface(FontCache.getInstance(this).getFont(FontCache.Font.REGULAR));
+        login_btn.setTypeface(FontCache.getInstance(this).getFont(FontCache.Font.MEDIUM));
+//        inputLayoutEmail.setHintTextAppearance(R.style.InActive);
+//        inputLayoutPassword.setHintTextAppearance(R.style.InActive);
+
+//        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+//                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+//                    checkLoginScreen();
+//                }
+//                return false;
+//            }
+//        });
+//        passwordEditText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Avenir-Roman.ttf"));
+        randString = Utils.randomChar();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login:
+                checkLoginScreen();
+                break;
+            case R.id.signUp:
+
+        }
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+        String username = userName.getText().toString().trim();
+        prefManager.setUserName(username);
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (username.isEmpty()) {
+            valid = false;
+            Utils.showAlert(this, "Please enter the username");
+        } else if (password.isEmpty()) {
+            valid = false;
+            Utils.showAlert(this, "Please enter the password");
+        }
+        return valid;
+    }
+
+    private void checkLoginScreen() {
+
+        final String username = userName.getText().toString().trim();
+        final String password = passwordEditText.getText().toString().trim();
+        prefManager.setUserPassword(password);
+
+        if (Utils.isOnline()) {
+            if (!validate())
+                return;
+            else if (prefManager.getUserName().length() > 0 && password.length() > 0) {
+                new ApiService(this).makeRequest("LoginScreen", Api.Method.POST, UrlGenerator.getLoginUrl(), loginParams(), "not cache", this);
+            } else {
+                Utils.showAlert(this, "Please enter your username and password!");
+            }
+        } else {
+            //Utils.showAlert(this, getResources().getString(R.string.no_internet));
+            AlertDialog.Builder ab = new AlertDialog.Builder(
+                    LoginScreen.this);
+            ab.setMessage("Internet Connection is not avaliable..Please Turn ON Network Connection OR Continue With Off-line Mode..");
+            ab.setPositiveButton("Settings",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                            int whichButton) {
+                            Intent I = new Intent(
+                                    android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                            startActivity(I);
+                        }
+                    });
+            ab.setNegativeButton("Continue With Off-Line",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,
+                                            int whichButton) {
+                            offline_mode(username, password);
+                        }
+                    });
+            ab.show();
+        }
+    }
+
+
+    public Map<String, String> loginParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put(AppConstant.KEY_SERVICE_ID, "login");
+
+
+        String random = Utils.randomChar();
+
+        params.put(AppConstant.USER_LOGIN_KEY, random);
+        Log.d("randchar", "" + random);
+
+        params.put(AppConstant.KEY_USER_NAME, prefManager.getUserName());
+        Log.d("user", "" + userName.getText().toString().trim());
+
+        String encryptUserPass = Utils.md5(passwordEditText.getText().toString().trim());
+        prefManager.setEncryptPass(encryptUserPass);
+        Log.d("md5", "" + encryptUserPass);
+
+        String userPass = encryptUserPass.concat(random);
+        Log.d("userpass", "" + userPass);
+        String sha256 = Utils.getSHA(userPass);
+        Log.d("sha", "" + sha256);
+
+        params.put(AppConstant.KEY_USER_PASSWORD, sha256);
+
+
+        Log.d("user", "" + userName.getText().toString().trim());
+
+
+        return params;
+    }
+
+    //The method for opening the registration page and another processes or checks for registering
+    private void ClickSignUp() {
+
+        android.support.v7.app.AlertDialog.Builder dialog = new android.support.v7.app.AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.register, null);
+        dialog.setView(dialogView);
+
+        reg_username = dialogView.findViewById(R.id.reg_username);
+        reg_password = dialogView.findViewById(R.id.reg_password);
+        reg_firstName = dialogView.findViewById(R.id.reg_firstName);
+        reg_lastName = dialogView.findViewById(R.id.reg_lastName);
+        reg_email = dialogView.findViewById(R.id.reg_email);
+        reg_confirmemail = dialogView.findViewById(R.id.reg_confirmemail);
+        register_btn = dialogView.findViewById(R.id.reg_register);
+        txtInLayoutRegPassword = dialogView.findViewById(R.id.txtInLayoutRegPassword);
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (reg_username.getText().toString().trim().isEmpty()) {
+
+                    reg_username.setError("Please fill out this field");
+                } else {
+                    //Here you can write the codes for checking username
+                }
+                if (reg_password.getText().toString().trim().isEmpty()) {
+                    txtInLayoutRegPassword.setPasswordVisibilityToggleEnabled(false);
+                    reg_password.setError("Please fill out this field");
+                } else {
+                    txtInLayoutRegPassword.setPasswordVisibilityToggleEnabled(true);
+                    //Here you can write the codes for checking password
+                }
+                if (reg_firstName.getText().toString().trim().isEmpty()) {
+
+                    reg_firstName.setError("Please fill out this field");
+                } else {
+                    //Here you can write the codes for checking firstname
+
+                }
+                if (reg_lastName.getText().toString().trim().isEmpty()) {
+
+                    reg_lastName.setError("Please fill out this field");
+                } else {
+                    //Here you can write the codes for checking lastname
+                }
+                if (reg_email.getText().toString().trim().isEmpty()) {
+
+                    reg_email.setError("Please fill out this field");
+                } else {
+                    //Here you can write the codes for checking email
+                }
+                if (reg_confirmemail.getText().toString().trim().isEmpty()) {
+
+                    reg_confirmemail.setError("Please fill out this field");
+                } else {
+                    //Here you can write the codes for checking confirmemail
+                }
+            }
+        });
+
+
+        dialog.show();
+
+
+    }
+
+    @Override
+    public void OnMyResponse(ServerResponse serverResponse) {
+        try {
+            JSONObject loginResponse = serverResponse.getJsonResponse();
+            String urlType = serverResponse.getApi();
+            String status = loginResponse.getString(AppConstant.KEY_STATUS);
+            String response = loginResponse.getString(AppConstant.KEY_RESPONSE);
+            String message = loginResponse.getString(AppConstant.KEY_MESSAGE);
+            if ("LoginScreen".equals(urlType)) {
+                if (status.equalsIgnoreCase("OK")) {
+                    if (response.equals("LOGIN_SUCCESS")) {
+                        String key = loginResponse.getString(AppConstant.KEY_USER);
+                        String user_data = loginResponse.getString(AppConstant.USER_DATA);
+                        String decryptedKey = Utils.decrypt(prefManager.getEncryptPass(), key);
+                        String userDataDecrypt = Utils.decrypt(prefManager.getEncryptPass(), user_data);
+                        Log.d("userdatadecry", "" + userDataDecrypt);
+                        jsonObject = new JSONObject(userDataDecrypt);
+
+                        Log.d("userdata", "" + prefManager.getDistrictCode() + prefManager.getBlockCode() + prefManager.getPvCode() + prefManager.getDistrictName() + prefManager.getBlockName() + prefManager.getPvName() + prefManager.getLevels());
+                        prefManager.setUserPassKey(decryptedKey);
+                        showHomeScreen();
+                    } else {
+                        if (response.equals("LOGIN_FAILED")) {
+                            Utils.showAlert(this, "Invalid UserName Or Password");
+                        }
+                    }
+                }
+
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void OnError(VolleyError volleyError) {
+        Utils.showAlert(this, "Login Again");
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        showHomeScreen();
+//    }
+
+    private void showHomeScreen() {
+        Intent intent = new Intent(LoginScreen.this,AppVersionActivity.class);
+
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+    }
+
+    public void offline_mode(String name, String pass) {
+        String userName = prefManager.getUserName();
+        String password = prefManager.getUserPassword();
+        if (name.equals(userName) && pass.equals(password)) {
+            showHomeScreen();
+        } else {
+            Utils.showAlert(this, "No data available for offline. Please Turn On Your Network");
+        }
+    }
+
+}
---
+++ app/src/main/java/com/nic/ODFPlusMonitoring/Activity/SplashScreen.java
@@ -0,0 +1,67 @@
+package com.nic.ODFPlusMonitoring.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.nic.ODFPlusMonitoring.Helper.AppVersionHelper;
+import com.nic.ODFPlusMonitoring.R;
+import com.nic.ODFPlusMonitoring.Session.PrefManager;
+
+public class SplashScreen extends AppCompatActivity implements
+        AppVersionHelper.myAppVersionInterface {
+    private TextView textView;
+    private Button button;
+    private static int SPLASH_TIME_OUT = 2000;
+    private PrefManager prefManager;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        prefManager = new PrefManager(this);
+        showSignInScreen();
+//        if (Utils.isOnline()) {
+//           checkAppVersion();
+//        } else {
+//            showSignInScreen();
+//
+//        }
+    }
+
+
+    private void showSignInScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, LoginScreen.class);
+
+                startActivity(i);
+                finish();
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    private void checkAppVersion() {
+        new AppVersionHelper(this, SplashScreen.this).callAppVersionCheckApi();
+    }
+
+    @Override
+    public void onAppVersionCallback(String value) {
+        if (value.length() > 0 && "Update".equalsIgnoreCase(value)) {
+            startActivity(new Intent(this, AppVersionActivity.class));
+            finish();
+            overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+        } else {
+            showSignInScreen();
+        }
+
+    }
+
+}
