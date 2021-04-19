76cee8280b32644747c08a2683ee765574e284d3
AchanthiSundar <tnrdmobile@gmail.com>
2021-04-19 12:14:17 +0530

    Implement AddParticipates in after Login and registration screen add some fields.

--- app/src/main/java/com/nic/ODFPlusMonitoring/Activity/AddParticipantsActivity.java
+++ app/src/main/java/com/nic/ODFPlusMonitoring/Activity/AddParticipantsActivity.java
@@ -18,12 +18,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -33,8 +35,17 @@ import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
 import com.nic.ODFPlusMonitoring.Adapter.AddParticipatesAdapterView;
+import com.nic.ODFPlusMonitoring.Adapter.CommonAdapter;
+import com.nic.ODFPlusMonitoring.Api.Api;
+import com.nic.ODFPlusMonitoring.Api.ApiService;
+import com.nic.ODFPlusMonitoring.Api.ServerResponse;
+import com.nic.ODFPlusMonitoring.Constant.AppConstant;
+import com.nic.ODFPlusMonitoring.Model.ODFMonitoringListValue;
 import com.nic.ODFPlusMonitoring.R;
+import com.nic.ODFPlusMonitoring.Session.PrefManager;
+import com.nic.ODFPlusMonitoring.Utils.UrlGenerator;
 import com.nic.ODFPlusMonitoring.Utils.Utils;
 
 import org.json.JSONArray;
@@ -47,7 +58,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class AddParticipantsActivity extends AppCompatActivity {
+public class AddParticipantsActivity extends AppCompatActivity implements Api.ServerResponseListener {
     Context context;
     private AlertDialog alert;
     FloatingActionButton floatingActionButton;
@@ -59,11 +70,16 @@ public class AddParticipantsActivity extends AppCompatActivity {
     Spinner designation;
     EditText m_name,m_mobile;
     ScrollView scrollView;
+    JSONObject appParticipatesJson;
+    private PrefManager prefManager;
+    ArrayList<ODFMonitoringListValue> designationList;
+    String selectedDesignation,selectedDesignationId;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_particption_screen);
         context=this;
+        prefManager = new PrefManager(this);
         floatingActionButton=findViewById(R.id.add_participants);
         home=findViewById(R.id.home_img);
 
@@ -85,6 +101,29 @@ public class AddParticipantsActivity extends AppCompatActivity {
                 showHomeScreen();
             }
         });
+
+
+    }
+    public void addParticipatesApiService() {
+        try {
+            new ApiService(this).makeJSONObjectRequest("AddParticipates", Api.Method.POST, UrlGenerator.getOpenUrl(), addParticipatesJsonParams(), "not cache", this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JSONObject addParticipatesJsonParams() {
+        JSONObject jsonObject=new JSONObject();
+        String authKey1 = Utils.encrypt(prefManager.getUserPassKey(), getResources().getString(R.string.init_vector), appParticipatesJson.toString());
+
+        try {
+            jsonObject.put(AppConstant.KEY_SERVICE_ID,"AddParticipates");
+            jsonObject.put(AppConstant.DATA_CONTENT,authKey1);
+
+        }catch (JSONException e){
+
+        }
+       return jsonObject;
     }
 
     public void setAdapter(){
@@ -166,7 +205,7 @@ public class AddParticipantsActivity extends AppCompatActivity {
     }
 
     public void imageWithDescription() {
-
+        appParticipatesJson=new JSONObject();
         final Dialog dialog = new Dialog(this,R.style.AppTheme);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.add_participates);
@@ -204,8 +243,27 @@ public class AddParticipantsActivity extends AppCompatActivity {
                         m_mobile=vv.findViewById(R.id.mobile_number);
                         m_name=vv.findViewById(R.id.name);
                         designation=vv.findViewById(R.id.designation_name);
+                        if(!m_mobile.getText().toString().equals("")&&!m_name.getText().toString().equals("")&&
+                        designation.getSelectedItemPosition()!=0) {
+                            if (Utils.isOnline()) {
+                                imageArray.put(i);
+                                imageArray.put(m_mobile.getText().toString());
+                                imageArray.put(m_name.getText().toString());
+                                imageArray.put(designation.getSelectedItemPosition());
+                                imageJson.put(imageArray);
+                            }
+                        }
+                        else {
+                            Utils.showAlert(AddParticipantsActivity.this,"Please fill all details in all view!");
+                        }
 
                     }
+                    try {
+                        appParticipatesJson.put("data",imageJson);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
 
                 }
                 dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
@@ -246,6 +304,20 @@ public class AddParticipantsActivity extends AppCompatActivity {
         m_name = (EditText) hiddenInfo.findViewById(R.id.name);
         m_mobile = (EditText) hiddenInfo.findViewById(R.id.mobile_number);
         final EditText myEditTextView = (EditText) hiddenInfo.findViewById(R.id.description);
+        loadDesignationList();
+        designation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedDesignation=(designationList.get(position).getGenderEn());
+                selectedDesignationId=(designationList.get(position).getGenderCode());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
         imageView_close.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -271,4 +343,55 @@ public class AddParticipantsActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void OnMyResponse(ServerResponse serverResponse) {
+            try {
+                JSONObject responseObj = serverResponse.getJsonResponse();
+                String urlType = serverResponse.getApi();
+                String status = null;
+                String response = null;
+                if ("AddParticipates".equals(urlType)) {
+                    status  = responseObj.getString(AppConstant.KEY_STATUS);
+                    response = responseObj.getString(AppConstant.KEY_RESPONSE);
+                    if (status.equalsIgnoreCase("OK")) {
+                    }
+                }
+
+
+
+            }catch (JSONException e){
+
+            }
+    }
+
+    private void loadDesignationList() {
+        try {
+            JSONArray jsonarray=new JSONArray();
+            if(jsonarray != null && jsonarray.length() >0) {
+                ODFMonitoringListValue ODFMonitoringListValue = new ODFMonitoringListValue();
+                ODFMonitoringListValue.setGenderEn("Select Designation");
+                designationList.add(ODFMonitoringListValue);
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject jsonobject = jsonarray.getJSONObject(i);
+                    String gender_code = jsonobject.getString("gender_code");
+                    String gender_name_en = (jsonobject.getString("gender_name_en"));
+                    String gender_name_ta = (jsonobject.getString("gender_name_ta"));
+                    ODFMonitoringListValue roadListValue = new ODFMonitoringListValue();
+                    roadListValue.setGenderCode(gender_code);
+                    roadListValue.setGenderEn(gender_name_en);
+                    roadListValue.setGenderTa(gender_name_ta);
+                    designationList.add(roadListValue);
+                }
+                designation.setAdapter(new CommonAdapter(this, designationList, "DesignationList"));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void OnError(VolleyError volleyError) {
+
+    }
 }
